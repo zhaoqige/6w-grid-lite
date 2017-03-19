b8d5bf5695d3f70a94482c599cd6d9e673a4e15a
qz <zhaoqige@163.com>
2017-03-19 15:15:40 -0400

    Write basic "ajax" answers; need to be verified on real devices.

--- lib/grid/Tool.lua
+++ lib/grid/Tool.lua
@@ -19,6 +19,11 @@ Tool.conf.dfl_flood_times = 10
 Tool.conf.ping_cmd_fmt = "ping %s -w %d"
 Tool.conf.dfl_ping_times = 4
 
+-- for spectrum/channel scan, quiet mode
+Tool.conf.chscan_file = '/tmp/.grid_lite_chscan'
+Tool.conf.chscan_cmd_fmt = 'gws_cs -q -r%d -b%d -e%d > %s'
+Tool.conf.chscan_abord_cmd = 'killall gws_cs; sleep 1; rm -f %s'
+Tool.conf.chscan_read_cmd_fmt = "cat %s | grep %d | awk -F ',' '{print $4}'"
 
 function Tool.Run()
 	cgi.save.init()
@@ -37,6 +42,16 @@ function Tool.Run()
 			local _times = fmt.http.find('times', _get)
 			local _bw = fmt.http.find('bw', _get)
 			_result = Tool.ops.flood(_to, _times, _bw)
+		elseif (_k == 'scan') then
+			local _ffrom = 470
+			local _fto = 700
+			local _rgn = 1
+			_result = Tool.ops.scan(_ffrom, _fto, rgn)
+		elseif (_k == 'scan_abord') then
+			_result = Tool.ops.scan_abord()
+		elseif (_k == 'scan_read') then
+			local _ch = fmt.http.find('ch', _get)
+			_result = Tool.ops.scan_read(_ch)
 		else
 			_result = string.format('unknown (%s)', _k or '[nil]')
 		end
@@ -84,6 +99,36 @@ function Tool.ops.ping(_to, _times)
 	return _result
 end
 
+function Tool.ops.scan(_from, _to, _rgn)
+	local _f = Tool.conf.chscan_file
+	local _fmt = Tool.conf.chscan_cmd_fmt
+	local _cmd = string.format(_fmt, _f, _rgn, _from, _to)
+	cmd.exec(_cmd)
+
+	local _result = '{"error": null, "result": "on"}'
+	return _result
+end
+
+function Tool.ops.scan_abord()
+	local _f = Tool.conf.chscan_file
+	local _fmt = Tool.conf.chscan_abord_cmd
+	local _cmd = string.format(_fmt, _f)
+	cmd.exec(_cmd)
+
+	local _result = '{"error": null, "result": "off"}'
+	return _result
+end
+
+function Tool.ops.scan_read(_ch)
+	local _f = Tool.conf.chscan_file
+	local _fmt = Tool.conf.chscan_read_cmd_fmt
+	local _cmd = string.format(_fmt, _f, _ch)
+	local _noise = cmd.exec(_cmd)
+
+	local _result_fmt = '{"error": null, "result": "ok", "data": { "ch": %d, "noise": %d }}'
+	local _result = string.format(_result_fmt, _ch, _noise or -110)
+	return _result
+end
 
 return Tool
 
